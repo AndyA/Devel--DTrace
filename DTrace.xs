3077a542f3ed3c5b1491e721aba4ec62260fa710
Andy Armstrong <andy@hexten.net>
2008-01-06 11:33:17 +0000

    It builds

--- DTrace.xs
+++ DTrace.xs
@@ -1,27 +1,47 @@
-#include "common.h"
-#include "tools.h"
+/* DTrace.xs */
+
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
+static int
+_runops_dtrace( pTHX ) {
+    // char *lastfile = NULL;
+    // int lastline = 0;
+
+    while ( ( PL_op = CALL_FPTR( PL_op->op_ppaddr ) ( aTHX ) ) ) {
+        PERL_ASYNC_CHECK(  );
+
+        // if ( interesting_op( PL_op->op_type ) ) {
+        //     /*fprintf(stderr, "%s, line %d\n", lastfile, lastline); */
+        //     NOTE_NEW_VARS( lastline, lastfile );
+        //     lastfile = CopFILE( cCOP );
+        //     lastline = CopLINE( cCOP );
+        // }
+    }
+
+    /*fprintf(stderr, "%s, line %d\n", lastfile, lastline); */
+
+    TAINT_NOT;
+    return 0;
+}
+
+static void
+_hook_runops( void ) {
+    if ( PL_runops != _runops_dtrace ) {
+        // runops_old = PL_runops;
+        PL_runops = _runops_dtrace;
+    }
+}
+
+/* *INDENT-OFF* */
 
 MODULE = Devel::DTrace PACKAGE = Devel::DTrace
-
 PROTOTYPES: ENABLE
 
 void
-_hook_runops()
+_dtrace_hook_runops()
 PPCODE:
 {
-    tools_hook_runops();
-}
-
-void
-_reset_counters()
-PPCODE:
-{
-    tools_reset_counters();
-}
-
-void
-_show_used()
-CODE:
-{
-    tools_show_used();
+    _hook_runops();
 }
--- lib/Devel/DTrace.pm
+++ lib/Devel/DTrace.pm
@@ -6,75 +6,39 @@ use strict;
 use warnings;
 use base 'DynaLoader';
 
+=head1 NAME
+
+Devel::DTrace - Enable dtrace probes for subroutine entry, exit
+
+=head1 SYNOPSIS
+
+  perl -MDevel::DTrace
+
+=head1 DESCRIPTION
+
+=cut
+
 BEGIN {
-    our $VERSION = '0.12';
+    our $VERSION = '0.01';
     bootstrap Devel::DTrace $VERSION;
-    _hook_runops();
-}
-
-INIT {
-    _reset_counters();
-}
-
-END {
-    _show_used();
+    _dtrace_hook_runops();
 }
 
 1;
 
 __END__
 
-=head1 NAME
-
-Devel::DTrace - indicate where leaked variables are coming from.
-
-=head1 SYNOPSIS
-
-  perl -MDevel::DTrace -e '{ my $foo; $foo = \$foo }'
-  leaked SV(0x528d0) from -e line 1
-  leaked SV(0x116a10) from -e line 1
-
-=head1 DESCRIPTION
-
-Devel::DTrace is a rewrite of Devel::LeakTrace. Like
-Devel::LeakTrace it uses the pluggable runops feature found in perl 5.6
-and later in order to trace SV allocations of a running program.
-
-At END time Devel::DTrace identifies any remaining variables, and
-reports on the lines in which the came into existence.
-
-Note that by default state is first recorded during the INIT phase.
-As such the module will not pay attention to any scalars created
-during BEGIN time.  This is intentional as symbol table aliasing is
-never released before the END times and this is most common in the
-implicit BEGIN blocks of C<use> statements.
-
-=head1 TODO
-
-Improve the documentation.
-
-Clustering of reports if they're from the same line.
-
-Stack backtraces to suspect lines.
 
 =head1 AUTHOR
 
 Andy Armstrong <andy@hexten.net>
 
-Originally based on code by Richard Clamp that carried this attribution:
-
-Richard Clamp <richardc@unixbeard.net> with portions of LeakTrace.xs
-taken from Nick Ing-Simmons' Devel::Leak module.
-
 =head1 LICENCE AND COPYRIGHT
 
-Copyright (c) 2007, Andy Armstrong C<< <andy@hexten.net> >>. All rights reserved.
+Copyright (c) 2008, Andy Armstrong C<< <andy@hexten.net> >>. All
+rights reserved.
 
-This module is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself. See L<perlartistic>.
-
-=head1 SEE ALSO
-
-L<Devel::Leak>, L<Devel::Cover>
+This module is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>.
 
 =cut
