25a8414115739f6a5f27abee9a7bdf8cb16c979c
Andy Armstrong <andy@hexten.net>
2008-01-11 19:56:17 +0000

    Tagging 0.07, version bump

--- lib/Devel/DTrace.pm
+++ lib/Devel/DTrace.pm
@@ -12,7 +12,7 @@ Devel::DTrace - Enable dtrace probes for subroutine entry, exit
 
 =head1 SYNOPSIS
 
-    $ perl -MDevel::DTrace prog.pl
+    $ perl -MDevel::DTrace prog.pl &
     $ ps -af | grep perl
     $ dtrace -p <PID> -s examples/subs-tree.d
 
@@ -52,6 +52,10 @@ C<Devel::DTrace> before you can connect to it with C<dtrace>.
 
 The C<dtperl> interpreter does not have this limitation.
 
+On Solaris C<dtperl> is statically linked and therefore probably not
+much use. Working out why I can't build a dynamic C<dtperl> is high on
+my todo list.
+
 =head2 Other Notes
 
 It's difficult to test the dtrace probes. To do so the tests would have
@@ -66,14 +70,10 @@ in some cases to
 
     $ DEVEL_DTRACE_RUNOPS_FAKE=1 dtperl someprog.pl
 
-=head2 Known Bugs
-
-Extra, spurious trace output is generated on exit from an eval.
-
 =cut
 
 BEGIN {
-    our $VERSION = '0.06';
+    our $VERSION = '0.07';
     bootstrap Devel::DTrace $VERSION;
     _dtrace_hook_runops();
 }
